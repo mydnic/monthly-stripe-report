7a9f01d68429138dd6f58bc2f9d26488afd0d8c3
Clément Rigo <rigoclement@mydnic.be>
2018-10-24 16:25:56 +0200

    add partials

---
+++ resources/views/partials/flash.blade.php
@@ -0,0 +1,9 @@
+@if(session('flash_notification'))
+
+    @foreach (session('flash_notification', collect())->toArray() as $message)
+        <notification level="{{ $message['level'] }}" message="{!! $message['message'] !!}"></notification>
+    @endforeach
+
+    {{ session()->forget('flash_notification') }}
+@endif
+<notification></notification>
